3c39eaff8db1a09d5a1e39f7e92906be14f787c2
Your Name <you@example.com>
2023-11-30 21:18:25 +0400

    First

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 Route::controller(UserAuthController::class)->prefix('auth')->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
-    Route::get('user', 'user')->middleware('auth:sanctum');
+    Route::get('user', 'user');
 });
 
    Route::get('announcement-types',[GlobalDataController::class,'announcementTypes']);
