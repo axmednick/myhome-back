e1566523389f525456916c124e854009ea7bec04
Your Name <you@example.com>
2024-03-02 16:59:42 +0400

    First

--- app/Models/AnnouncementMetroStation.php
+++ app/Models/AnnouncementMetroStation.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class AnnouncementMetroStation extends Model
 {
     use HasFactory;
+
+    public function metro_station(){
+        return $this->belongsTo(MetroStation::class);
+    }
 }
