4497cd4a249e32416ff35d67fb8bfc601fab490a
Your Name <you@example.com>
2024-03-10 18:40:16 +0400

    First

--- app/Http/Resources/AnnouncementResource.php
+++ app/Http/Resources/AnnouncementResource.php
@@ -33,7 +33,7 @@ class AnnouncementResource extends JsonResource
             'house_area' => $this->house_area,
             'description' => $this->description,
             'price' => number_format($this->price, 0, ',', ' '),
-            'price_per_square' => $this->price!=0 ?  round($this->price / $this->house_area) : 0,
+            'price_per_square' => $this->house_area!=0 ?  round($this->price / $this->house_area) : 0,
             'user_id' => $this->user_id,
             'address' => AnnouncementAddressResource::make($this->address),
             'images' => ImageResource::collection($this->getMedia('image')),
