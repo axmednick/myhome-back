1267b6146fd67aaa319e589f5dc673c15ec7a16a
Your Name <you@example.com>
2024-03-10 15:01:56 +0400

    First

--- app/Services/AnnouncementService.php
+++ app/Services/AnnouncementService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+
 use App\Models\Announcement;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -18,70 +19,124 @@ class AnnouncementService
         }
 
 
-        if ($request->announcementType){
+        if ($request->announcementType) {
             $announcements->where('announcement_type_id', $request->announcementType);
 
         }
 
-        if ($request->roomCountMin){
+        if ($request->room_ids) {
 
-            $announcements->where('room_count','>=', $request->roomCountMin);
-
+            if ($request->room_ids == "5+") {
+                $announcements->where('room_count', '>', 5);
+            } else {
+                $announcements->whereIn('room_count', $request->room_ids);
+            }
         }
-        if ($request->roomCountMax){
-            $announcements->where('room_count','<=', $request->roomCountMax);
 
-        }
-
-        if ($request->apartmentType){
+        if ($request->apartmentType) {
             $announcements->where('apartment_type_id', $request->apartmentType);
 
         }
 
-        if ($request->minPrice){
-            $announcements->where('price','>=', $request->minPrice);
+        if ($request->minPrice) {
+            $announcements->where('price', '>=', $request->minPrice);
 
         }
 
-        if ($request->maxPrice){
-            $announcements->where('price','<=', $request->maxPrice);
+        if ($request->maxPrice) {
+            $announcements->where('price', '<=', $request->maxPrice);
 
         }
 
-        if ($request->minArea){
-            $announcements->where('house_area','>=', $request->minArea);
+        if ($request->minArea) {
+            $announcements->where('house_area', '>=', $request->minArea);
 
         }
 
-        if ($request->maxArea){
-            $announcements->where('house_area','<=', $request->maxArea);
+        if ($request->maxArea) {
+            $announcements->where('house_area', '<=', $request->maxArea);
 
         }
 
-        if ($request->city){
-            $announcements->whereHas('address',function ($query) use($request){
-                $query->where('city_id',$request->city);
+        if ($request->cities) {
+            $announcements->whereHas('address', function ($query) use ($request) {
+                $query->whereIn('city_id', $request->cities);
             });
         }
 
-        if ($request->regions){
-            $announcements->whereHas('address',function ($query) use($request){
-                $query->whereIn('region_id',$request->regions);
+        if ($request->regions) {
+            $announcements->whereHas('address', function ($query) use ($request) {
+                $query->whereIn('region_id', $request->regions);
             });
         }
-        if ($request->villages){
-            $announcements->whereHas('address',function ($query) use($request){
-                $query->whereIn('village_id',$request->villages);
+        if ($request->villages) {
+            $announcements->whereHas('address', function ($query) use ($request) {
+                $query->whereIn('village_id', $request->villages);
             });
         }
+
+        if ($request->metro_stations) {
+            $announcements->whereHas('address', function ($query) use ($request) {
+                $query->whereIn('metro_station_id', $request->metro_stations);
+            });
+        }
+
+        if ($request->keyword) {
+            $announcements->
+            where('id', $request->keyword)->
+            orWhere('description', 'like', '%' . $request->keyword . '%');
+        }
+
+        if ($request->user_type){
+            $announcements->whereHas('user',function ($query) use ($request){
+               $query->where('user_type',$request->user_type);
+            });
+        }
+
+        if ($request->maxFloor) {
+            $announcements->where('floor', '<=', $request->maxFloor);
+
+        }
+
+        if ($request->minFloor) {
+            $announcements->where('floor', '>=', $request->minFloor);
+
+        }
+        if ($request->is_repaired) {
+            $announcements->where('is_repaired',  $request->is_repaired);
+
+        }
+
+        if ($request->property_document){
+            $announcements->where('document_id',1);
+        }
+        if ($request->credit_possible){
+            $announcements->where('credit_possible',1);
+        }
+
+        if ($request->in_credit){
+            $announcements->where('in_credit',1);
+        }
+
+        if ($request->looking_roommate){
+            $announcements->where('looking_roommate',1);
+        }
+
+        if ($request->rental_client_types){
+            $announcements->whereHas('rental_client_types',function ($query) use ($request){
+                $query->whereIn('client_type_for_rent_id',$request->rental_client_types);
+            });
+        }
+
 
         return $announcements;
     }
 
 
-    public function announcementsByUser($userId){
+    public function announcementsByUser($userId)
+    {
 
-        $announcements = Announcement::where('user_id',$userId)->query();
+        $announcements = Announcement::where('user_id', $userId)->query();
 
         return $announcements;
     }
