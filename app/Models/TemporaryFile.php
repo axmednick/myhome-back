fc22fab642ac84f61306cc4fc49d983e3ff7886e
Your Name <you@example.com>
2024-03-30 17:55:18 +0400

    First

--- app/Models/TemporaryFile.php
+++ app/Models/TemporaryFile.php
@@ -4,10 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Image\Manipulations;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class TemporaryFile extends Model implements HasMedia
 {
     use HasFactory,InteractsWithMedia;
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('image')
+            ->registerMediaConversions(function (Media $media) {
+                $this
+                    ->addMediaConversion('thumb')
+                    ->width(500)
+                    ->height(500)
+                    ->optimize()
+                    ->performOnCollections('image');
+
+                $this
+                    ->addMediaConversion('watermarked')
+                    ->watermark(public_path('watermark.png'))
+                    ->watermarkPosition(Manipulations::POSITION_CENTER) // Watermarkın mərkəzi mövqeyi
+                    ->width(1000) // Width of the image after adding watermark
+                    ->height(1000) // Height of the image after adding watermark
+                    ->optimize()
+                    ->watermarkOpacity(25)
+                    ->performOnCollections('image');
+            });
+    }
 }
--- config/app.php
+++ config/app.php
@@ -169,6 +169,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
+
     ])->toArray(),
 
     /*
@@ -184,6 +186,8 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
+        'Image' => Intervention\Image\Facades\Image::class,
+
     ])->toArray(),
 
 ];
