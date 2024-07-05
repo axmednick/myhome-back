2627e646cfda21fcf1887bdb1a53f796a4e2de22
Your Name <you@example.com>
2024-07-06 02:25:27 +0400

    Refactor: Cache user announcement count for authenticated users

--- app/Http/Controllers/BonusController.php
+++ app/Http/Controllers/BonusController.php
@@ -15,7 +15,7 @@ class BonusController extends Controller
         if (UserBonus::where('user_id', $user->id)->exists()) {
             return false;
         }
-        $currentBonus = Bonus::where('announcement_count', '>', $user->announcements()->count())->first();
+        $currentBonus = Bonus::where('announcement_count', '<', $user->announcements()->count())->first();
         return BonusResource::make($currentBonus);
     }
 
