43a61492b3b745fc1272d16a00c081a5b4792edd
Your Name <you@example.com>
2024-07-05 22:47:15 +0400

    Update password hash to use request input

--- app/Http/Controllers/Auth/UserAuthController.php
+++ app/Http/Controllers/Auth/UserAuthController.php
@@ -39,7 +39,7 @@ class UserAuthController extends Controller
 
 
         $input = $request->all();
-        $input['password'] = Hash::make($request->password);
+        $input['password'] = $request->password;
 
         $user = User::create($input);
         $success['token'] = $user->createToken('MyApp')->plainTextToken;
