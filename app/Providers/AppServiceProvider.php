7fe79066f0848edef8e8931689d62c95b4213de2
Your Name <you@example.com>
2024-07-09 01:28:18 +0400

    Add Slack

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use App\Models\Announcement;
+use App\Models\User;
 use App\Models\UserBonus;
 use App\Observers\AnnouncementObserver;
 use App\Observers\UserBonusObserver;
+use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,6 +27,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Announcement::observe(AnnouncementObserver::class);
         UserBonus::observe(UserBonusObserver::class);
+        User::observe(UserObserver::class);
 
     }
 }
