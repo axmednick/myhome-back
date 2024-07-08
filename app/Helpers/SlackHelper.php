34754df751860fbf6f26c789b3ecc0087dd3d98b
Your Name <you@example.com>
2024-07-09 01:35:39 +0400

    Add Slack

--- app/Helpers/SlackHelper.php
+++ app/Helpers/SlackHelper.php
@@ -2,18 +2,23 @@
 
 namespace App\Helpers;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
 
 class SlackHelper
 {
     public static function sendMessage($message)
     {
-        $webhookUrl = 'https://hooks.slack.com/services/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';
+        $client = new Client();
 
-        $response = Http::post($webhookUrl, [
-            'text' => 'Myhome: '.$message
+        $response = $client->post('https://hooks.slack.com/services/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', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'text' => "Myhome: {$message}",
+            ],
         ]);
 
-        return $response->successful();
     }
 }
