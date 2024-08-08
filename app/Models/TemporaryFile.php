14987ab930258d22d7027fbf5a41dd68cf45ad35
Your Name <you@example.com>
2024-08-08 11:24:45 +0400

    Add Slack

--- app/Models/TemporaryFile.php
+++ app/Models/TemporaryFile.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Image\Manipulations;
+
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
@@ -19,23 +20,23 @@ class TemporaryFile extends Model implements HasMedia
             ->registerMediaConversions(function (Media $media) {
                 $this
                     ->addMediaConversion('thumb')
-                    ->width(344)
-                    ->height(244)
-                    ->fit('crop', 344, 244) // Crop işlemiyle tam boyutlandırma
+                    ->width(500)
+                    ->height(500)
                     ->optimize()
                     ->performOnCollections('image');
 
                 $this
                     ->addMediaConversion('watermarked')
                     ->watermark(public_path('watermark.png'))
-                    ->watermarkPosition('center')
-                    ->width(1000) // Watermark ekledikten sonra resmin genişliği
-                    ->height(1000) // Watermark ekledikten sonra resmin yüksekliği
+                    ->watermarkPosition('center') // Watermarkın mərkəzi mövqeyi
+                    ->width(1000) // Width of the image after adding watermark
+                    ->height(1000) // Height of the image after adding watermark
                     ->optimize()
                     ->watermarkOpacity(20)
                     ->performOnCollections('image');
             });
         $this->addMediaConversion('original')->nonQueued();
+
     }
 
 }
