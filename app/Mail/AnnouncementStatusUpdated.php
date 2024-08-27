6a9492f43fcf7f5cee1c9920b7f480a049970f4a
Your Name <you@example.com>
2024-08-27 16:36:29 +0400

    Meta tags

--- app/Mail/AnnouncementStatusUpdated.php
+++ app/Mail/AnnouncementStatusUpdated.php
@@ -45,7 +45,7 @@ class AnnouncementStatusUpdated extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'mail.announcementStatusUpdate',
         );
     }
 
