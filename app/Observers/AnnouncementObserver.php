e979e1c6dde270db54647975fa68ab3e3bb3a71e
Your Name <you@example.com>
2024-08-29 00:42:54 +0400

    Store bug

--- app/Observers/AnnouncementObserver.php
+++ app/Observers/AnnouncementObserver.php
@@ -12,19 +12,19 @@ use Illuminate\Support\Facades\Mail;
 
 class AnnouncementObserver
 {
-/*    public function created(Announcement $announcement)
+    public function created(Announcement $announcement)
     {
 
-        //TelegramHelper::sendMessage($announcement->user->name . ' created a new announcement: ' . $announcement->id);
+       // TelegramHelper::sendMessage($announcement->user->name . ' created a new announcement: ' . $announcement->id);
 
-        //Mail::to($announcement->user->email)->queue(new AnnouncementCreated($announcement));
+        Mail::to($announcement->user->email)->queue(new AnnouncementCreated($announcement));
 
-    }*/
+    }
 
-/*    public function updated(Announcement $announcement)
+    public function updated(Announcement $announcement)
     {
         if ($announcement->isDirty('status') && $announcement->status == 1) {
             Mail::to($announcement->user->email)->queue(new AnnouncementStatusUpdated($announcement));
         }
-    }*/
+    }
 }
