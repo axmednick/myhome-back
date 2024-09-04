c2728a071b491a05d3fd60fb16fecd49e2bfdc8f
Your Name <you@example.com>
2024-09-04 22:02:07 +0400

    Store bug

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         \Log::error('Running');
-        $schedule->command('announcements:check-pending')->everyFiveSeconds();
+        $schedule->command('announcements:check-pending')->everyTenMinutes();
 
     }
 
