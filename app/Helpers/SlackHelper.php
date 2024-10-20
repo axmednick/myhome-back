0b04839e4bc2fdb6b26b5401362c13971e063860
Your Name <you@example.com>
2024-10-20 16:04:49 +0400

    Store bug

--- app/Helpers/SlackHelper.php
+++ app/Helpers/SlackHelper.php
@@ -2,21 +2,23 @@
 
 namespace App\Helpers;
 
-use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
 
 class SlackHelper
 {
     public static function sendMessage($message)
     {
+        // Slack Webhook URL
+        $slackUrl = 'https://hooks.slack.com/services/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';
 
-        $slackUrl = 'https://hooks.slack.com/services/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';
-
-        $message = [
-            'text' =>$message,
+        // Mesajı JSON formatına çeviririk
+        $payload = [
+            'text' => $message,
         ];
 
-        return $response = Http::post($slackUrl, $message);
-
+        // POST sorğusu göndəririk JSON formatında
+        return Http::withHeaders([
+            'Content-Type' => 'application/json',
+        ])->post($slackUrl, $payload);
     }
 }
