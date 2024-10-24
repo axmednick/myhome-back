37970456a925d5316d612669bfccaa0e46edf011
Your Name <you@example.com>
2024-10-24 13:16:47 +0400

    Store bug

--- app/Console/Commands/DashinmakEmlakUsersParserCommand.php
+++ app/Console/Commands/DashinmakEmlakUsersParserCommand.php
@@ -32,13 +32,13 @@ class DashinmakEmlakUsersParserCommand extends Command
         $maxPages = 1885;
         $progressBar = $this->output->createProgressBar($maxPages);
 
-        for ($page = 136; $page <= $maxPages; $page++) {
+        for ($page = 1; $page <= $maxPages; $page++) {
             $html = $client->load('https://yeniemlak.az/elan/axtar?seher%5B%5D=0&metro%5B%5D=0&qiymet=&qiymet2=&mertebe=&mertebe2=&otaq=&otaq2=&sahe_m=&sahe_m2=&sahe_s=&sahe_s2=$page'.$page);
             $announcements = $html->find('.list');
 
             foreach ($announcements as $announcement) {
                 $announcementUrl = 'https://yeniemlak.az'.$announcement->find('a', 0)->getAttribute('href');
-                Log::error($announcementUrl);
+
                 sleep(5);
                 $announcementDetailPage = $client->load($announcementUrl);
                 $userName = $announcementDetailPage->find('.ad', 0)->text();
@@ -46,12 +46,16 @@ class DashinmakEmlakUsersParserCommand extends Command
                 $userPhoneDiv = $announcementDetailPage->find('.tel', 0);
                 $userPhone = str_replace('/tel-show/', '', $userPhoneDiv->find('img', 0)->getAttribute('src'));
 
-                DataAgent::create([
-                    'name'=>$userName,
-                    'type'=>$userType,
-                    'phone'=>$userPhone,
-                    'site'=>'yeniemlak.az'
-                ]);
+                if (!DataAgent::where('phone',$userPhone)->first()){
+                    DataAgent::create([
+                        'name'=>$userName,
+                        'type'=>$userType,
+                        'phone'=>$userPhone,
+                        'site'=>'yeniemlak.az'
+                    ]);
+                }
+
+
             }
 
             $progressBar->advance();
