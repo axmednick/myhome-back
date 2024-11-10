8fc81fec1fba66a9685fd0334108cfe83252f988
Your Name <you@example.com>
2024-11-10 13:16:25 +0400

    Add updateUserType method to ProfileController
    Implemented a new method in ProfileController to update the user's type. Added validation to ensure the user_type field is required. Also, registered a new API route for this method in api.php.

--- app/Http/Resources/AnnouncementResource.php
+++ app/Http/Resources/AnnouncementResource.php
@@ -17,51 +17,56 @@ class AnnouncementResource extends JsonResource
      */
     public function toArray($request)
     {
-        $date = Carbon::parse($this->created_at)->format('d ') . trans('content.' . Carbon::parse($this->created_at)->format('F')) . Carbon::parse($this->created_at)->format(' Y');
+        try {
+            $date = Carbon::parse($this->created_at)->format('d ') . trans('content.' . Carbon::parse($this->created_at)->format('F')) . Carbon::parse($this->created_at)->format(' Y');
 
-
-        return [
-            'id' => $this->id,
-            'announcement_type' => AnnouncementTypeResource::make($this->announcement_type),
-            'property_type' => PropertyTypeResource::make($this->property_type),
-            'apartment_type' => ApartmentTypeResource::make($this->apartment_type),
-            'area' => $this->area,
-            'room_count' => $this->room_count,
-            'rental_type' => $this->rental_type,
-            'floor_count' => $this->floor_count,
-            'floor' => $this->floor,
-            'house_area' => $this->house_area,
-            'description' => $this->description,
-            //'price' => number_format($this->price, 0, ',', ' '),
-            //'price_per_square' => $this->house_area!=0 ?  round($this->price / $this->house_area) : 0,
-            'user_id' => $this->user_id,
-            'address' => AnnouncementAddressResource::make($this->address),
-            'images' => ImageResource::collection($this->getMedia('image')),
-            'main_image' => $this->getFirstMediaUrl('main') ?: $this->getFirstMediaUrl('image'),
-            'main_image_thumb' => $this->getFirstMediaUrl('main', 'thumb_main') ?: $this->getFirstMediaUrl('image', 'thumb'),
-
-            'user' => $this->user,
-            'short_title' => $this->shortTitle(),
-            'title' => $this->title(),
-            'short_details' => $this->shortDetails(),
-            'date' => $date,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-            "formatted_date" => DateHelper::formatCreatedAt($this->created_at),
-            'view_count' => $this->view_count,
-            'is_active' => $this->is_active,
-            'is_favorite'=>Favorite::where('announcement_id', $this->id) ->where('user_id',auth('sanctum')->id())->exists(),
-            'supplies'=>AnnouncementSuppliesResource::collection($this->supplies),
-            'client_types_for_rent'=>RentalClientTypesResource::collection($this->rental_client_types),
-            'looking_roommate'=>$this->looking_roommate,
-            'credit_possible'=>$this->credit_possible,
-            'in_credit'=>$this->in_credit,
-            'metro_stations'=>AnnouncementMetroStationsResource::collection($this->metro_stations),
-            'status'=>$this->status,
-            'document_id'=>$this->document_id,
-            'is_repaired'=>$this->is_repaired
-
-        ];
+            return [
+                'id' => $this->id,
+                'announcement_type' => AnnouncementTypeResource::make($this->announcement_type),
+                'property_type' => PropertyTypeResource::make($this->property_type),
+                'apartment_type' => ApartmentTypeResource::make($this->apartment_type),
+                'area' => $this->area,
+                'room_count' => $this->room_count,
+                'rental_type' => $this->rental_type,
+                'floor_count' => $this->floor_count,
+                'floor' => $this->floor,
+                'house_area' => $this->house_area,
+                'description' => $this->description,
+                'price' => isset($this->price) ? number_format($this->price, 0, ',', ' ') : null, // Problemi burada izah etdik
+                'price_per_square' => $this->house_area ? round($this->price / $this->house_area) : 0,
+                'user_id' => $this->user_id,
+                'address' => AnnouncementAddressResource::make($this->address),
+                'images' => ImageResource::collection($this->getMedia('image')),
+                'main_image' => $this->getFirstMediaUrl('main') ?: $this->getFirstMediaUrl('image'),
+                'main_image_thumb' => $this->getFirstMediaUrl('main', 'thumb_main') ?: $this->getFirstMediaUrl('image', 'thumb'),
+                'user' => $this->user,
+                'short_title' => $this->shortTitle(),
+                'title' => $this->title(),
+                'short_details' => $this->shortDetails(),
+                'date' => $date,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+                "formatted_date" => DateHelper::formatCreatedAt($this->created_at),
+                'view_count' => $this->view_count,
+                'is_active' => $this->is_active,
+                'is_favorite' => Favorite::where('announcement_id', $this->id)->where('user_id', auth('sanctum')->id())->exists(),
+                'supplies' => AnnouncementSuppliesResource::collection($this->supplies),
+                'client_types_for_rent' => RentalClientTypesResource::collection($this->rental_client_types),
+                'looking_roommate' => $this->looking_roommate,
+                'credit_possible' => $this->credit_possible,
+                'in_credit' => $this->in_credit,
+                'metro_stations' => AnnouncementMetroStationsResource::collection($this->metro_stations),
+                'status' => $this->status,
+                'document_id' => $this->document_id,
+                'is_repaired' => $this->is_repaired
+            ];
+        } catch (\Exception $e) {
+            Log::error("Announcement ID {$this->id} has an error: " . $e->getMessage());
+            return [
+                'id' => $this->id,
+                'error' => 'Price information could not be retrieved'
+            ];
+        }
     }
 
 
