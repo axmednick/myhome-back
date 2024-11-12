608bf2e57f2bc48180995b5fb668a9323bafa296
Your Name <you@example.com>
2024-11-12 22:40:33 +0400

    Add updateUserType method to ProfileController
    Implemented a new method in ProfileController to update the user's type. Added validation to ensure the user_type field is required. Also, registered a new API route for this method in api.php.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        \Log::error('Running');
+
         $schedule->command('announcements:check-pending')->everyTenMinutes();
 
     }
