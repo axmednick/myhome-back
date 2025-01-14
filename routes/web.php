75a161c00874a04043696f6597cdbd0ed4d3f44b
Your Name <you@example.com>
2025-01-14 11:19:43 +0400

    Agents

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@ Route::get('/', [\App\Http\Controllers\DefaultController::class,'index']);
 Route::get('/auth/{driver}/redirect', [App\Http\Controllers\GoogleLoginController::class, 'redirect'])->name('google.redirect');
 Route::get('/auth/{driver}/callback', [App\Http\Controllers\GoogleLoginController::class, 'handleCallback'])->name('google.callback');
 Route::get('/data',function (){
-    $listings = \App\Models\Listing::where('ads_count','>',1)->select('name','phone')->orderBy('ads_count','desc')->get();
+    $listings = \App\Models\Listing::where('ads_count','>',1)->orderBy('ads_count','desc')->get();
     foreach ($listings as $listing){
-        echo '<div>' . $listing->name .' '.$listing->phone.'</div>';
+        echo '<div>' . $listing->name .' '.$listing->phone.'</div> Elan sayı <b>'.$listing->ads_count.'</b>';
     }
 });
