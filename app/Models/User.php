6fbca35f5bbf5594ff1877768d702d9971d85fed
Your Name <you@example.com>
2025-01-18 23:25:54 +0400

    Agency

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,7 +24,8 @@ class User extends Authenticatable implements HasMedia
         'email',
         'password',
         'phone',
-        'agency_id'
+        'agency_id',
+        'user_type',
     ];
 
     /**
