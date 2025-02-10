580153d504e2368c15b03439f359f81bacf57e5e
Your Name <you@example.com>
2025-02-10 23:47:50 +0400

    Payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -69,6 +69,7 @@ class PaymentController extends Controller {
     {
         DB::beginTransaction();
 
+        dd($request->all());
         try {
             $payload = $request->input('payload');
             $orderId = $payload['orderID'];
