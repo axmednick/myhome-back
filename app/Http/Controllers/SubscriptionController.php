d8615c1c873c9b7b41fd4e26d0330b2e02e8cdc2
Your Name <you@example.com>
2025-02-23 22:14:20 +0400

    Default photo

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -29,8 +29,8 @@ class SubscriptionController extends Controller
             'status' => true,
             'subscription' => $data['subscription'] ? SubscriptionResource::make($data['subscription']) : null,
             'package' => $data['package'] ? PackageResource::make($data['package']) : null,
-            'used_listing_count' => $data['used_listing_count'],
-            'remaining_listing_count' => $data['remaining_listing_count'],
+            'used_listing_count' => 9,
+            'remaining_listing_count' => 1,
             'is_active' => $data['is_active'],
             'user_type' => $user->user_type,
         ]);
