34ce600f680f6d97cdf02e4b3ac37e3f91eec209
Ahmad Rustamov <ahmad@rustamov.az>
2025-03-16 18:04:32 +0400

    Renew free package command

--- app/Console/Commands/DeactivateExpiredSubscriptions.php
+++ app/Console/Commands/DeactivateExpiredSubscriptions.php
@@ -3,23 +3,42 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Services\SubscriptionService;
+use App\Models\Subscription;
+use Carbon\Carbon;
 
 class DeactivateExpiredSubscriptions extends Command
 {
     protected $signature = 'subscriptions:deactivate-expired';
-    protected $description = 'Deactivates expired subscriptions';
-    protected $subscriptionService;
-
-    public function __construct(SubscriptionService $subscriptionService)
-    {
-        parent::__construct();
-        $this->subscriptionService = $subscriptionService;
-    }
+    protected $description = 'Deactivates expired subscriptions and renews free package subscriptions';
 
     public function handle()
     {
-        $this->subscriptionService->deactivateExpiredSubscriptions();
-        $this->info("Expired subscriptions deactivated.");
+        $expiredSubscriptions = Subscription::where('end_date', '<', Carbon::now())
+            ->where('is_active', true)
+            ->get();
+
+        if ($expiredSubscriptions->isNotEmpty()) {
+            Subscription::whereIn('id', $expiredSubscriptions->pluck('id'))
+                ->update(['is_active' => false]);
+            $this->info("Deactivated " . $expiredSubscriptions->count() . " expired subscriptions.");
+        }
+
+        $freeSubscriptions = Subscription::where('package_id', 4)
+            ->where('end_date', '<', Carbon::now()) // Vaxtı bitmiş olmalıdır
+            ->where('is_active', false) // Yalnız deaktiv olmuşları götür
+            ->get();
+
+        if ($freeSubscriptions->isNotEmpty()) {
+            foreach ($freeSubscriptions as $subscription) {
+                $subscription->update([
+                    'start_date' => Carbon::now(),
+                    'end_date' => Carbon::now()->addDays(30), // 30 gün əlavə edirik
+                    'is_active' => true
+                ]);
+                $this->info("Renewed free subscription for user_id: {$subscription->user_id} or agency_id: {$subscription->agency_id}");
+            }
+        }
+
+        $this->info('Expired subscriptions processed.');
     }
 }
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,8 +40,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \App\Http\Middleware\LogApiRequest::class,
-
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
