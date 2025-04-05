ceac07980eb051361aa78b3bd942a7ff403a3222
Ahmad Rustamov <ahmad@rustamov.az>
2025-04-05 23:56:02 +0400

    TemporaryFile

--- app/Services/AnnouncementService.php
+++ app/Services/AnnouncementService.php
@@ -169,16 +169,13 @@ class AnnouncementService
         }
 
         if ($request->has('metro')) {
-
-
             $metroStations = is_array($request->query('metro')) ? $request->query('metro') : [$request->query('metro')];
 
-
-            $announcements->whereHas('metro_stations',function ($query) use ($metroStations) {
-
-                $query->whereIn('metro_station_id', $metroStations);
+            $announcements->whereHas('metro_stations', function ($query) use ($metroStations) {
+                $query->whereIn('metro_station_id',$metroStations);
             });
         }
+
 
 
 
